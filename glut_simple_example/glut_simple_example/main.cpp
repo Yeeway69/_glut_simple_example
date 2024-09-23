b569675551dac801f9d1f605e64df03cc202580a
Pep Cots <pec@milestone.dk>
2024-09-23 12:40:16 +0200

    typo

--- glut_simple_example/glut_simple_example/main.cpp
+++ glut_simple_example/glut_simple_example/main.cpp
@@ -10,7 +10,7 @@ using vec3 = glm::dvec3;
 
 static const ivec2 WINDOWS_SIZE(512, 512);
 
-static void draw_triangle(const u8vec4& color, const const vec3& center, double size) {
+static void draw_triangle(const u8vec4& color, const vec3& center, double size) {
 	glColor4ub(color.r, color.g, color.b, color.a);
 	glBegin(GL_TRIANGLES);
 	glVertex3d(center.x, center.y + size, center.z);
