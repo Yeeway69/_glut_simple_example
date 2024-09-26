5e277cd048be6055b3a90d10e183c8324dee02c1
Yeeway69 <yeyiwei1@gmail.com>
2024-09-26 15:08:48 +0200

    drew triangles and rotated

--- glut_simple_example/glut_simple_example/main.cpp
+++ glut_simple_example/glut_simple_example/main.cpp
@@ -22,6 +22,44 @@ static void draw_triangle(const u8vec4& color, const vec3& center, double size) 
 static void display_func() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	draw_triangle( u8vec4(255, 0, 0, 255), vec3(0.0, 0.0, 0.0), 0.5);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, static_cast<double>(WINDOWS_SIZE.x) / WINDOWS_SIZE.y, 0.1, 100.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 2.0, 10.0, 0.0, 2.0, 0.0, 0.0, 1.0, 0.0);
+
+	//(0.0)(1,0)(0.1)(1,1)
+	//draw one quad coordinate
+	vec3 a(0, 0, 0);
+	vec3 b(1, 0, 0);
+	vec3 c(0, 1, 0);
+	vec3 d(1, 1, 0);
+
+
+	static double angle = 0.0;
+	angle += 0.1;
+
+	glRotated(angle, 0.0, 1.0, 0.0);
+
+	glBegin(GL_TRIANGLES);
+	
+
+	
+	glColor3ub(200, 20, 0);
+	glVertex3d(a.x, a.y, a.z);
+	glVertex3d(b.x, b.y, b.z);
+	glVertex3d(c.x, c.y, c.z);
+
+	glColor3ub(0, 20, 200);
+	glVertex3d(b.x, b.y, b.z);
+	glVertex3d(c.x, c.y, c.z);
+	glVertex3d(d.x, d.y, d.z);
+
+	glEnd();
+
 	glutSwapBuffers();
 }
 
@@ -42,6 +80,7 @@ int main(int argc, char* argv[]) {
 
 	// Set Glut callbacks
 	glutDisplayFunc(display_func);
+	glutIdleFunc(glutPostRedisplay);
 
 	// Enter glut main loop
 	glutMainLoop();
