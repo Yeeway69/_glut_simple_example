1ea37de678d4ea3c14517445a9e014ee6047c0fd
Pep Cots <pec@milestone.dk>
2024-10-01 00:50:04 +0200

    Added Transform and Camera classes

--- glut_simple_example/glut_simple_example/main.cpp
+++ glut_simple_example/glut_simple_example/main.cpp
@@ -2,16 +2,13 @@
 #include <glm/glm.hpp>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include "Camera.h"
 using namespace std;
 
-using u8vec4 = glm::u8vec4;
-using ivec2 = glm::ivec2;
-using vec3 = glm::dvec3;
+static Camera camera;
 
-static const ivec2 WINDOW_SIZE(512, 512);
-
-static void draw_triangle(const u8vec4& color, const vec3& center, double size) {
-	glColor4ub(color.r, color.g, color.b, color.a);
+static void draw_triangle(const glm::u8vec3& color, const vec3& center, double size) {
+	glColor3ub(color.r, color.g, color.b);
 	glBegin(GL_TRIANGLES);
 	glVertex3d(center.x, center.y + size, center.z);
 	glVertex3d(center.x - size, center.y - size, center.z);
@@ -19,30 +16,82 @@ static void draw_triangle(const u8vec4& color, const vec3& center, double size) 
 	glEnd();
 }
 
+static void draw_floorGrid(int size, double step) {
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINES);
+	for (double i = -size; i <= size; i += step) {
+		glVertex3d(i, 0, -size);
+		glVertex3d(i, 0, size);
+		glVertex3d(-size, 0, i);
+		glVertex3d(size, 0, i);
+	}
+	glEnd();
+}
+
 static void display_func() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	draw_triangle( u8vec4(255, 0, 0, 255), vec3(0.0, 0.0, 0.0), 0.5);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixd(&camera.view()[0][0]);
+
+	draw_floorGrid(16, 0.25);
+	draw_triangle( Colors::Red, vec3(-1, 0.25, 0), 0.5);
+	draw_triangle(Colors::Green, vec3(0, 0.5, 0.25), 0.5);
+	draw_triangle(Colors::Blue, vec3(1, -0.5, -0.25), 0.5);
+
 	glutSwapBuffers();
 }
 
 static void init_opengl() {
 	glewInit();
+
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+	glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+	glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
+
+	glEnable(GL_POLYGON_SMOOTH);
+	glEnable(GL_LINE_SMOOTH);
+	glEnable(GL_POINT_SMOOTH);
+
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+
+	glEnable(GL_DEPTH_TEST);
+
 	glClearColor(0.5, 0.5, 0.5, 1.0);
+}
+
+static void reshape_func(int width, int height) {
+	glViewport(0, 0, width, height);
+	camera.aspect = static_cast<double>(width) / height;
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixd(&camera.projection()[0][0]);
+}
+
+static void mouseWheel_func(int wheel, int direction, int x, int y) {
+	camera.transform().translate( vec3(0, 0, direction * 0.1));
 }
 
 int main(int argc, char* argv[]) {
 	// Iniit window and context
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(WINDOW_SIZE.x, WINDOW_SIZE.y);
+	glutInitWindowSize(1280, 720);
 	glutCreateWindow("Glut Simple Example");
 
 	// Init OpenGL
 	init_opengl();
 
+	// Init camera
+	camera.transform().pos() = vec3(0, 1, 4);
+	camera.transform().rotate(glm::radians(180.0), vec3(0, 1, 0));
+
 	// Set Glut callbacks
 	glutDisplayFunc(display_func);
 	glutIdleFunc(glutPostRedisplay);
+	glutReshapeFunc(reshape_func);
+	glutMouseWheelFunc(mouseWheel_func);
 
 	// Enter glut main loop
 	glutMainLoop();
